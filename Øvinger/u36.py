ce9b7c4b14d38996ee49215f2355ffc5841f9f2f
tranquilite <mathias.moe@tranquilite.com>
2021-09-09 19:29:55 +0200

    Rydder litt spacing, og legger til alternativ metode på o8

--- Øvinger/u36.py
+++ Øvinger/u36.py
@@ -14,7 +14,7 @@ def test_opg(crib: dict) -> None:
     for oppgave in crib:
         start = time.time()
         call = oppgave[0]
-        print(call.__name__+' ', end='')
+        print(call.__name__+'\t', end='')
         for problem in oppgave[1]:
             try:
                 assert call(*problem[0:-1]) == problem[-1]
@@ -22,8 +22,8 @@ def test_opg(crib: dict) -> None:
                 if DEBUG is True:
                     errlog.append(
                         # as an aside - this unwrap is stupid
-                        f'F {call.__name__} {call(*problem[0:-1])} ' +
-                        f'!= {problem[-1]}')
+                        f'F {call.__name__}\t{call(*problem[0:-1])}' +
+                        f' != {problem[-1]}')
                     print('F', end='')
                 else:
                     print('F', end='')
@@ -68,7 +68,18 @@ def o7(spent: int, trip: int) -> float:
 
 
 def o8(num: int) -> tuple:
-    """REEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"""
+    """REEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+    Does it with-a smoothbrain math"""
+    h, m, s = 0, 0, 0
+    h = int(num/60/60)
+    m = int(num/60 - h*60)
+    s = int(num - h*60*60 - m*60)
+    return (h, m, s)
+
+
+def o8_2(num: int) -> tuple:
+    """REEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+    Does it with a loopeti-loop"""
     h, m, s = 0, 0, 0
     while num != 0:
         s += 1
@@ -93,7 +104,7 @@ def o10(time: int) -> int:
 
 
 if __name__ == '__main__':
-    ovinger = [o1, o2, o3, o4, o5, o6, o7, o8, o9, o10]
+    ovinger = [o1, o2, o3, o4, o5, o6, o7, o8, o8_2, o9, o10]
     oppgaver = [
         [(30, 10, 40), (-30, 10, -20), (0, 0, 0)],
         [(3, 9, 27), (-30, 10, -300), (0, 9, 0)],
@@ -102,7 +113,8 @@ if __name__ == '__main__':
         [(2.00, 12.5664), (100.64, 31819.3103), (150.00, 70685.7750)],
         [(5, 6, 7, 8, (5, 8)), (3, 0, 7, 8, (0, 8)), (5, 6, -7, 8, (-7, 8))],
         [(10, 85, 70.833), (2, 92, 15.333), (22, 67, 122.833)],
-        [(556, (0, 9, 16)), (1, (0, 0, 1)), (140153, (38, 55, 53))],
+        [(556, (0, 9, 16)), (1, (0, 0, 1)), (140153, (38, 55, 53))],  # o8
+        [(556, (0, 9, 16)), (1, (0, 0, 1)), (140153, (38, 55, 53))],  # o8_2
         [(500, 35.0, 12.286), (2254, 124.4, 18.119), (4554, 464.6, 9.802)],
         [(30, 60), (110, 220), (7, 14)]
     ]
