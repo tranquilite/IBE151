bf11e919b9684a96b42d417eadda9fc84c78473e
tranquilite <mathias.moe@gmail.com>
2021-09-13 21:10:41 +0200

    Justerer for et ttilfelle hvor funksjonen som ble wrappet ga en liste

--- Exercises/hjelpere.py
+++ Exercises/hjelpere.py
@@ -1,6 +1,10 @@
 def fprint(func):
     def wrapper(*args, **kwargs):
         ret = func(*args, **kwargs)
-        print(f'>{func.__name__} {ret}')
+        if isinstance(ret, list):
+            for _ in ret:
+                print(_)
+        else:
+            print(f'>{func.__name__} {ret}')
         return ret
     return wrapper
