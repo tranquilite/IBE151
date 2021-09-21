2e2be6a6a38335fe9a1ab9d6edfbaadfb1dbd0ac
tranquilite <mathias.moe@gmail.com>
2021-09-21 18:36:41 +0200

    Legger til en ny hjelper-funksjon som skal fjerne unødvendige input()-kall

--- Exercises/hjelpere.py
+++ Exercises/hjelpere.py
@@ -8,3 +8,28 @@ def fprint(func):
             print(f'>{func.__name__} {ret}')
         return ret
     return wrapper
+
+
+def ask_input(query, types):
+    """Er du lei av alle oppgavene som vil at du skal bruke evig mange input()?
+    Look no further! Her er satan sjæl."""
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            inputs = []
+            for _ in range(len(query) if len(query) == len(types) else 0):
+                while True:
+                    try:
+                        sends = types[_](input(query[_] + ': '))
+                    except ValueError:
+                        print(f'Input is not valid for ',
+                              f'type {types[_].__name__}')
+                    else:
+                        if len(query) == 1:
+                            inputs = sends
+                        else:
+                            inputs.append(sends)
+                        break
+
+            return func(*args, **{'inputs': inputs, **kwargs})
+        return wrapper
+    return decorator
